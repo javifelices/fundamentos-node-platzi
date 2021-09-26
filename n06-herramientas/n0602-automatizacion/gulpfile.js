9050282de522bd48f1fa31468ce3480370c369e2
Javi Felices <javifelices@gmail.com>
2021-09-26 12:52:05 +0200

    Añadir automatización con gulp

---
+++ n06-herramientas/n0602-automatizacion/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const server = require('gulp-server-livereload');
+
+gulp.task('build', (cb) => {
+    console.log('Construyendo el sitio');
+    setTimeout(cb, 1200);
+});
+
+gulp.task('serve', (cb) => {
+    gulp.src('www').pipe(
+        server({
+            livereload: true,
+            open: true,
+            port: 8001,
+        })
+    );
+});
+
+gulp.task('default', gulp.series('build', 'serve'));
